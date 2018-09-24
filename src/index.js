eea947ff4ab6d7b4bb6e40842c867b7a6a0d9454
Zerovers <Zeromon19941994@mail.ru>
2018-09-24 20:25:28 +0300

    emplemented check

--- src/index.js
+++ src/index.js
@@ -1,3 +1,39 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  let openElement = []; 
+  let closeElement = [];
+  
+  bracketsConfig.forEach(function(few) {
+     openElement.push(few[0]);
+     closeElement.push(few[1]);
+  });
+ for (let i = 0; i < str.length; i++) {
+     let open = openElement.indexOf(str[i]);
+     let close = closeElement.indexOf(str[i]);
+     if (open !== -1) {
+       if(close !== -1 ) {
+          if(str[i] !== stack[stack.length -1]) {
+            stack.push(str[i]);
+            }  else {
+            stack.pop();
+         }
+         continue;
+       }
+       stack.push(str[i]);
+       continue;
+    }
+    if(close !== -1) {
+        let lastSymb = stack.pop();
+        let indxcOpen = openElement.indexOf(lastSymb);
+        let indxClose = close;
+        if ( indxcOpen !== indxClose) {
+          return false;
+        }
+    }
+  }
+  if( stack.length !== 0) {
+    return false;
+  }
+  return true;
 }
+
